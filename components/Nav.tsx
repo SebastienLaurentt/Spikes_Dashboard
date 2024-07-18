6458f4d45085c60cebcdadf988b9bd24437a1128
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-19 00:47:32 +0200

    refacto and use useState to update css off selectedTab

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,92 +1,72 @@
-import { ChevronDown, Crown, SearchIcon } from "lucide-react";
+"use client";
+
+import { ChevronDown, Crown } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Progress } from "./ui/progress";
 
 const Nav = () => {
+  const [selectedTab, setSelectedTab] = useState<string>("Leads");
+
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+  };
+
   return (
-    <div className="w-[326px] h-[800px] px-8 flex flex-col ">
+    <div className="w-[326px] h-[800px] px-8 flex flex-col">
       <div className="space-y-6 flex flex-col">
         <Image src="/images/icon.png" alt="logo" width={32} height={32} />
-        <Input 
-        placeholder="Search"
-        hasSearchIcon
-        hasShortCutIcon
-        searchIconSize={20}
-      />
+        <Input
+          placeholder="Search"
+          hasSearchIcon
+          hasShortCutIcon
+          searchIconSize={20}
+        />
       </div>
-      <div className="py-3">
+      <div className="py-2">
         <span className="text-primary-foreground text-[14px]">Navigation</span>
-        <ul className="text-sm font-bold gap-y-2 flex flex-col py-2">
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/homeIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Home</span>
-          </li>
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/clientIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Clients</span>
-          </li>
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/leadsIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Leads</span>
-          </li>
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/dealsIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Deals</span>
-          </li>
+        <ul className="text-sm font-bold gap-y-[1px] flex flex-col">
+          {["Home", "Clients", "Leads", "Deals"].map((item) => (
+            <li
+              key={item}
+              className={`flex flex-row gap-x-3 py-2 px-3 cursor-pointer hover:bg-primary ${
+                selectedTab === item ? "bg-primary rounded-[6px]" : ""
+              }`}
+              onClick={() => handleTabClick(item)}
+            >
+              <Image
+                src={`/images/${item.toLowerCase()}Icon.svg`}
+                width={18}
+                height={18}
+                alt={`${item} icon`}
+              />
+              <span>{item}</span>
+            </li>
+          ))}
         </ul>
       </div>
-      <div className="py-3">
+      <div className="py-2">
         <span className="text-primary-foreground text-[14px]">Your Lists</span>
-        <ul className="text-sm font-bold gap-y-2 flex flex-col py-2">
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/inboundIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Inbound</span>
-          </li>
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/outboundIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Outbound</span>
-          </li>
-          <li className="flex flex-row gap-x-3 px-3 ">
-            <Image
-              src="/images/adsIcon.svg"
-              width={18}
-              height={18}
-              alt="home icon"
-            />
-            <span>Ads</span>
-          </li>
+        <ul className="text-sm font-bold gap-y-[1px] flex flex-col">
+          {["Inbound", "Outbound", "Ads"].map((item) => (
+            <li
+              key={item}
+              className={`flex flex-row gap-x-3 py-2 px-3 cursor-pointer hover:bg-primary ${
+                selectedTab === item ? "bg-primary rounded-[6px]" : ""
+              }`}
+              onClick={() => handleTabClick(item)}
+            >
+              <Image
+                src={`/images/${item.toLowerCase()}Icon.svg`}
+                width={18}
+                height={18}
+                alt={`${item} icon`}
+              />
+              <span>{item}</span>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="py-3">
@@ -96,17 +76,19 @@ const Nav = () => {
               src="/images/notificationsIcon.svg"
               width={18}
               height={18}
-              alt="home icon"
+              alt="notifications icon"
             />
             <span>Notification</span>
-            <span className="bg-accent text-primary w-[30px] h-[16px] flex flex-row items-center justify-center text-[8px] rounded-[4px]">3</span>
+            <span className="bg-accent text-primary w-[30px] h-[16px] flex flex-row items-center justify-center text-[8px] rounded-[4px]">
+              3
+            </span>
           </li>
-          <li className="flex flex-row gap-x-3 px-3 ">
+          <li className="flex flex-row gap-x-3 px-3">
             <Image
               src="/images/settingsIcon.svg"
               width={18}
               height={18}
-              alt="home icon"
+              alt="settings icon"
             />
             <span>Settings</span>
           </li>
@@ -118,9 +100,9 @@ const Nav = () => {
             src="/images/dealsIcon.svg"
             width={18}
             height={18}
-            alt="home icon"
+            alt="deals icon"
           />
-          <span className="text-[14px] font-bold ">Deals</span>
+          <span className="text-[14px] font-bold">Deals</span>
         </div>
         <div>
           <Progress value={66} />
@@ -128,7 +110,6 @@ const Nav = () => {
             5 Deals left from 30 Leads
           </span>
         </div>
-
         <Button className="gap-x-2">
           <Crown /> Upgrade to pro
         </Button>
@@ -136,7 +117,7 @@ const Nav = () => {
       <div className="bg-primary border mt-2 flex flex-row p-2 items-center rounded-[12px] gap-x-[11px]">
         <Image
           src="/images/profil.png"
-          alt="ads"
+          alt="profile"
           width={47}
           height={47}
           className="rounded-[4px]"
