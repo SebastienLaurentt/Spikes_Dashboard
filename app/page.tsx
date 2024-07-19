40dec0ad551678d4f560da5e3349277d205f5183
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-19 16:34:04 +0200

    refacto table

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import StatueCircle from "@/components/StatueCircle";
 import { Button } from "@/components/ui/button";
@@ -17,8 +17,87 @@ import { ArrowDown, Download, FolderPlus } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
+type StatusType = "Pending" | "Cancelled" | "Ongoing" | "Waiting" | "Completed";
+
+interface TableRow {
+  date: string;
+  object: string;
+  company: {
+    name: string;
+    image: string;
+  };
+  status: StatusType;
+  amount: string;
+}
+
+const tableData: TableRow[] = [
+  {
+    date: "Jan. 24",
+    object: "Product Design : Payme...",
+    company: { name: "Stripe Inc.", image: "/images/Stripe.png" },
+    status: "Pending",
+    amount: "7.800 $USD",
+  },
+  {
+    date: "Jan. 24",
+    object: "App Redesign : Onboar...",
+    company: { name: "Github Corp.", image: "/images/GitHub.png" },
+    status: "Cancelled",
+    amount: "12.800 $USD",
+  },
+  {
+    date: "Dec. 23",
+    object: "Pitch Deck B2B",
+    company: { name: "Amazon", image: "/images/Amazon.png" },
+    status: "Ongoing",
+    amount: "14.000 $USD",
+  },
+  {
+    date: "Oct. 23",
+    object: "Mobile App, UX Audit",
+    company: { name: "Steam", image: "/images/Steam.png" },
+    status: "Waiting",
+    amount: "2.000 $USD",
+  },
+  {
+    date: "Oct. 23",
+    object: "Splash Screen Illustrator",
+    company: { name: "Adobe LLC.", image: "/images/Adobe.png" },
+    status: "Completed",
+    amount: "5.500 $USD",
+  },
+  {
+    date: "Oct. 23",
+    object: "Features Add",
+    company: { name: "The Browser Company", image: "/images/Arc.png" },
+    status: "Pending",
+    amount: "14.500 $USD",
+  },
+  {
+    date: "Sept. 23",
+    object: "Brand Guidelines",
+    company: { name: "Figma", image: "/images/Figma.png" },
+    status: "Completed",
+    amount: "21.500 $USD",
+  },
+  {
+    date: "Sept. 23",
+    object: "New messages UX",
+    company: { name: "Slack", image: "/images/Slack.png" },
+    status: "Ongoing",
+    amount: "1.900 $USD",
+  },
+  {
+    date: "Sept. 23",
+    object: "Landing page",
+    company: { name: "Opensea", image: "/images/Opensea.png" },
+    status: "Pending",
+    amount: "2.300 $USD",
+  },
+];
+
 export default function Home() {
-  const [selectedFilter, setSelectedFilter] = useState<string>('Completed (3)'); 
+  const [selectedFilter, setSelectedFilter] = useState<string>("Completed (3)");
 
   const handleFilterClick = (filter: string) => {
     setSelectedFilter(filter);
@@ -28,7 +107,7 @@ export default function Home() {
     <main className="bg-primary w-full flex-1 rounded-l-[12px] border pl-10 pt-8">
       <div className="flex flex-col gap-y-5">
         <h2>Deals</h2>
-        <ul className=" flex flex-row gap-x-5 text-[14px] border-b w-fit font-medium">
+        <ul className="flex flex-row gap-x-5 text-[14px] border-b w-fit font-medium">
           {[
             "Completed (3)",
             "Pending (10)",
@@ -121,7 +200,7 @@ export default function Home() {
             <tr className="bg-background border-b text-[12px] flex flex-row justify-start">
               <th className="py-3 px-6 font-medium w-[152px] flex flex-row space-x-3 items-center">
                 <Checkbox id="terms" />
-                <span className="space-x-1  flex flex-row items-center">
+                <span className="space-x-1 flex flex-row items-center">
                   <span>Add</span> <ArrowDown size={16} />
                 </span>
               </th>
@@ -132,189 +211,33 @@ export default function Home() {
             </tr>
           </thead>
           <tbody className="text-[14px] font-medium ">
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Jan. 24</span>
-              </td>
-              <td className="py-4 px-6 w-[237px]">Product Design : Payme...</td>
-              <td className="py-4 px-6 w-[237px] flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Stripe.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Stripe Inc.</span>
-              </td>
-              <td className="py-4 px-6 w-[237px] flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Pending" />
-                Pending
-              </td>
-              <td className="py-4 px-6 w-[237px]">7.800 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                {" "}
-                <Checkbox id="terms" /> <span>Jan. 24</span>
-              </td>
-              <td className="py-4 px-6 w-[237px]">App Redesign : Onboar...</td>
-              <td className="py-4 px-6 w-[237px] flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/GitHub.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Github Corp.</span>
-              </td>
-              <td className="py-4 px-6 w-[237px] flex flex-row items-center gap-x-2.5 ">
-                <StatueCircle status="Cancelled" /> Cancelled
-              </td>
-              <td className="py-4 px-6 w-[237px]">12.800 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                {" "}
-                <Checkbox id="terms" /> <span>Dec. 23</span>
-              </td>
-              <td className="py-4 px-6 w-[237px]">Pitch Deck B2B</td>
-              <td className="py-4 px-6 w-[237px] flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Amazon.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Amazon</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Ongoing" /> Ongoing
-              </td>
-              <td className="py-4 w-[237px] px-6">14.000 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Oct. 23</span>
-              </td>
-              <td className="py-4 w-[237px] px-6">Mobile App, UX Audit</td>
-              <td className="py-4 w-[237px] px-6 flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Steam.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Steam</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Waiting" /> Waiting for Confirmation
-              </td>
-              <td className="py-4 w-[237px] px-6">2.000 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Oct. 23</span>
-              </td>
-              <td className="py-4 w-[237px] px-6">Splash Screen Illustrator</td>
-              <td className="py-4 w-[237px] px-6 flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Adobe.png"
-                  alt="Stripe Img"
-                  height={32}
-                  width={32}
-                  className="rounded-full"
-                />
-                <span> Adobe LLC.</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Completed" /> Completed
-              </td>
-              <td className="py-4 w-[237px] px-6">5.500 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Oct. 23</span>
-              </td>
-              <td className="py-4 w-[237px] px-6">Features Add</td>
-              <td className="py-4 w-[237px] px-6 flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Arc.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> The Browser Company</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Pending" /> Pending
-              </td>
-              <td className="py-4  w-[237px] px-6">14.500 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Sept. 23</span>
-              </td>
-              <td className="py-4 w-[237px] px-6">Brand Guidelines</td>
-              <td className="py-4 w-[237px] px-6 flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Figma.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Figma</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Completed" /> Completed
-              </td>
-              <td className="py-4 w-[237px] px-6">21.500 $USD</td>
-            </tr>
-            <tr className="border-b flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Sept. 23</span>
-              </td>
-              <td className="py-4 w-[237px] px-6">New messages UX</td>
-              <td className="py-4 w-[237px] px-6 flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Slack.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Slack</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Ongoing" /> Ongoing
-              </td>
-              <td className="py-4 w-[237px] px-6">1.900 $USD</td>
-            </tr>
-            <tr className="flex flex-row justify-start">
-              <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
-                <Checkbox id="terms" /> <span>Sept. 23</span>
-              </td>
-              <td className="py-4 w-[237px] px-6">Landing page</td>
-              <td className="py-4 w-[237px] px-6 flex flex-row gap-x-2 items-center">
-                <Image
-                  src="/images/Opensea.png"
-                  alt="Stripe Img"
-                  height={28}
-                  width={28}
-                  className="rounded-full"
-                />
-                <span> Opensea</span>
-              </td>
-              <td className="py-4 w-[237px] px-6 flex flex-row items-center gap-x-2.5">
-                <StatueCircle status="Pending" /> Pending
-              </td>
-              <td className="py-4 w-[237px] px-6">2.300 $USD</td>
-            </tr>
+            {tableData.map((row, index) => (
+              <tr
+                key={index}
+                className={`flex flex-row justify-start ${
+                  index !== tableData.length - 1 ? "border-b" : ""
+                }`}
+              >
+                <td className="py-4 px-6 gap-x-3 flex flex-row items-center w-[152px]">
+                  <Checkbox id="terms" /> <span>{row.date}</span>
+                </td>
+                <td className="py-4 px-6 w-[237px]">{row.object}</td>
+                <td className="py-4 px-6 w-[237px] flex flex-row gap-x-2 items-center">
+                  <Image
+                    src={row.company.image}
+                    alt={`${row.company.name} Img`}
+                    height={28}
+                    width={28}
+                    className="rounded-full"
+                  />
+                  <span>{row.company.name}</span>
+                </td>
+                <td className="py-4 px-6 w-[237px] flex flex-row items-center gap-x-2.5">
+                  <StatueCircle status={row.status} /> {row.status}
+                </td>
+                <td className="py-4 px-6 w-[237px]">{row.amount}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
