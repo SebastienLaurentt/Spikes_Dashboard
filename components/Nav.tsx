6038b329f2e37de58e4d0aa1d151ddf4541cad7f
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-26 16:41:53 +0200

    fix typo import issue

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -54,7 +54,7 @@ const Nav = () => {
         </ul>
       </div>
 
-      {/* Your Lists Tab */}
+      {/* Your List Tab */}
       <div className="py-2">
         <span className="text-primary-foreground text-[14px]">Your Lists</span>
         <ul className="text-sm font-bold gap-y-[1px] flex flex-col">
